cf8fd2a0bfa6611344e8533964020063977937a2
Justin A Ryan <justizin@justiz.in>
2021-06-23 11:52:58 -0700

    rename the tests to match the book, even though they don't really make sense.

--- integ-test-example/tests/integration_test1.rs
+++ integ-test-example/tests/integration_test1.rs
@@ -1,5 +1,15 @@
 use integ_test_example;
 #[test]
-fn integration_test1() {
+fn files_test1() {
     assert_ne!(integ_test_example::get_process_id(), 0, "Invalid process id");
+}
+
+#[test]
+fn files_test2() {
+    assert_eq!(1+1, 2);
+}
+
+#[test]
+fn process_test1() {
+    assert!(true);
 }
