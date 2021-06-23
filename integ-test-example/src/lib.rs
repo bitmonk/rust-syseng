ae557a45672e173f8fecf8673b0c007df419ad31
Justin A Ryan <justizin@justiz.in>
2021-06-23 12:17:35 -0700

    add inline docs to integ-test-example/src/lib.rs

--- integ-test-example/src/lib.rs
+++ integ-test-example/src/lib.rs
@@ -1,5 +1,11 @@
+//! This is a library that contains functions related to
+//! dealing with processes,
+//! and makes these tasks more convenient
+
 use std::process;
 
+/// This function gets the process ID of the current
+/// executable.  It returns a non-zero number
 pub fn get_process_id() -> u32 {
     process::id()
 }
